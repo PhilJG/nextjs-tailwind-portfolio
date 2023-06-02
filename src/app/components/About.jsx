3a5956d792fd3b9672dbd5666445d679edd67328
Phil J Gray <phl.j.gray@gmail.com>
2023-06-02 01:06:13 -0700

    Another edit

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
             <div className='bg-light-about-image md:grid-span-2 sm:grid-span-1 dark:bg-dark-about-image dark:bg-top  bg-left-center bg-center bg-no-repeat rounded-2xl'>
             </div>
             <div className="grid-span-1 sm:grid-span-2">
-                <h1 id="about" className="font-serif font-semibold text-4xl">Hi, I`m Phil</h1>
+                <h1 id="about" className="font-serif font-semibold text-4xl">Hi, I&rsquo;m Phil</h1>
                 <p className="text-xl pt-10">
 
                     I&rsquo;m a 32-year-old self-taught web developer and former clean-tech founder based in Victoria BC. Beginning in
