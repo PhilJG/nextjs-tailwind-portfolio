30f4766ce7845f713973b52ec4769a3dc2f4d152
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-10-12 00:24:10 -0700

    move social links to right side on contact form on desktop

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -1,69 +1,101 @@
-import React from 'react';
-import { FaGithub, FaLinkedin } from "react-icons/fa"
-import { SecondaryButton } from './Button';
-
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { SecondaryButton } from "./Button";
 
 const ContactSection = () => {
-    return (
-        <section >
-            <h1 className="font-serif font-semibold text-4xl  md:text-right sm:text-left text-center py-3">Get in Touch</h1>
-            <div className="contact__content">
-                <p className='text-lg text-center md:text-right'>
-                    I’d love to hear about what you’re working on and how I could help.
-                    <br />
-                    Please do feel free to check out my online profiles below and get in touch.
-                </p>
-                <ul className="flex justify-end lg:justify-start pt-4">
-                    <li>
-                        <a href="https://github.com/PhilJG" className=" text-dark-blue dark:text-white ">
-                            <FaGithub size={40} className=' hover:fill-cyan dark:fill-white dark:hover:fill-yellow-500 fill-dark' />
-                        </a>
-                    </li>
+  return (
+    <section>
+      <h1 className="font-serif font-semibold text-4xl  md:text-right sm:text-left text-center py-3">
+        Get in Touch
+      </h1>
+      <div className="contact__content">
+        <p className="text-lg text-center md:text-right">
+          I’d love to hear about what you’re working on and how I could help.
+          <br />
+          Please do feel free to check out my online profiles below and get in
+          touch.
+        </p>
+        <ul className="flex justify-end  pt-4">
+          <li>
+            <a
+              href="https://github.com/PhilJG"
+              className=" text-dark-blue dark:text-white "
+            >
+              <FaGithub
+                size={40}
+                className=" hover:fill-cyan dark:fill-white dark:hover:fill-yellow-500 fill-dark"
+              />
+            </a>
+          </li>
 
-                    <li>
-                        <a href="https://www.linkedin.com/in/phil-j-gray/" className="dark:text-white text-dark-blue  hover:bg-cyan">
-                            <FaLinkedin size={40} className=' hover:fill-cyan dark:fill-white dark:hover:fill-yellow-500 fill-dark' />
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </section>
-    );
+          <li>
+            <a
+              href="https://www.linkedin.com/in/phil-j-gray/"
+              className="dark:text-white text-dark-blue  hover:bg-cyan"
+            >
+              <FaLinkedin
+                size={40}
+                className=" hover:fill-cyan dark:fill-white dark:hover:fill-yellow-500 fill-dark"
+              />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 const ContactPage = () => {
-    return (
-        <section id="contact">
+  return (
+    <section id="contact">
+      <form
+        action="https://formsubmit.co/me@philjgray.ca"
+        method="POST"
+        className="form"
+      >
+        <label htmlFor="">Name</label>
+        <br />
+        <input
+          className="border font-bold text-lg  rounded-md px-2 py-1 w-full dark:bg-transparent"
+          placeholder="Jane Appleseed"
+          type="text"
+          name="name"
+          required
+        />
+        <br />
+        <label htmlFor="">Email</label>
+        <br />
 
-            <form action="https://formsubmit.co/me@philjgray.ca" method="POST" className="form">
-                <label htmlFor="">Name</label>
-                <br />
-                <input className="border font-bold text-lg  rounded-md px-2 py-1 w-full dark:bg-transparent" placeholder="Jane Appleseed" type="text" name="name" required />
-                <br />
-                <label htmlFor="">Email</label>
-                <br />
-
-                <input className="border font-bold text-lg  rounded-md px-2 py-1 w-full dark:bg-transparent" placeholder="example@email.com" type="email" name="email" required />
-                <br />
-                <label htmlFor="">Message</label>
-                <br />
-                <textarea className="border font-bold text-lg  rounded-md px-2 py-1 w-full dark:bg-transparent" placeholder="How can I help" type="text" name="message" required></textarea>
-                <input type="hidden" name="_next" value="https://philjgray.ca/" />
-                <br />
-                <SecondaryButton type={'submit'} text={'Send Message'} />
-            </form>
-        </section>
-    );
+        <input
+          className="border font-bold text-lg  rounded-md px-2 py-1 w-full dark:bg-transparent"
+          placeholder="example@email.com"
+          type="email"
+          name="email"
+          required
+        />
+        <br />
+        <label htmlFor="">Message</label>
+        <br />
+        <textarea
+          className="border font-bold text-lg  rounded-md px-2 py-1 w-full dark:bg-transparent"
+          placeholder="How can I help"
+          type="text"
+          name="message"
+          required
+        ></textarea>
+        <input type="hidden" name="_next" value="https://philjgray.ca/" />
+        <br />
+        <SecondaryButton type={"submit"} text={"Send Message"} />
+      </form>
+    </section>
+  );
 };
 
 export default function Contact() {
-    return (
-
-        <div className="grid gap-16 md:grid-cols-2 grid-cols-1 py-16">
-            <ContactSection />
-            <ContactPage />
-        </div>
-
-
-    );
+  return (
+    <div className="grid gap-16 md:grid-cols-2 grid-cols-1 py-16">
+      <ContactSection />
+      <ContactPage />
+    </div>
+  );
 }
